37e46b2160c4f1254bdefc51fa8a2c9ca382763b
Juu-dev <Thong.pm772002@gmail.com>
2022-09-17 15:23:19 +0700

    Complete Toggle function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,90 @@
 import FormBuilder from "./components/form_builder/form_builder_JS/formBuilder";
 import Preview from "./components/form_builder/form_builder_JS/Preview";
+import { Button } from "semantic-ui-react";
+import $ from "jquery";
+
 import FormJson from "./components/form_builder/form_builder_JS/FormJson";
 
-// import "./App.css";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [render, setRender] = useState("");
+
+  useEffect(() => {
+    const formBuilder = $(".FormBuilder");
+    const preview = $(".Preview");
+    const formJson = $(".FormJson");
+
+    console.log("render", render);
+    console.log("FormBuilder", formBuilder[0].classList);
+    console.log("Preview", preview[0].classList);
+    console.log("FormJson", formJson[0].classList);
+
+    switch (render) {
+      case "formBuilder":
+        formBuilder[0].classList.remove("None_Display");
+        preview[0].classList.add("None_Display");
+        formJson[0].classList.add("None_Display");
+        formBuilder[0].classList.add("Display_Content");
+        break;
+      case "preview":
+        formBuilder[0].classList.add("None_Display");
+        preview[0].classList.remove("None_Display");
+        formJson[0].classList.add("None_Display");
+        preview[0].classList.add("Display_Content");
+        break;
+      case "formJson":
+        formBuilder[0].classList.add("None_Display");
+        preview[0].classList.add("None_Display");
+        formJson[0].classList.remove("None_Display");
+        formJson[0].classList.add("Display_Content");
+        break;
+
+      default:
+        break;
+    }
+  }, [render]);
+
   return (
-    <>
-      <FormBuilder />
-      <Preview />
-      <FormJson />
-    </>
+    <div>
+      <div className="Button_Router">
+        <Button
+          primary
+          onClick={() => {
+            setRender("formBuilder");
+          }}
+        >
+          FormBuilder
+        </Button>
+        <Button
+          primary
+          onClick={() => {
+            setRender("preview");
+          }}
+        >
+          Preview
+        </Button>
+        <Button
+          primary
+          onClick={() => {
+            setRender("formJson");
+          }}
+        >
+          FormJson
+        </Button>
+      </div>
+      <div className={"FormBuilder Display_Content"}>
+        <FormBuilder />
+      </div>
+      <div className={"Preview None_Display"}>
+        <Preview />
+      </div>
+      <div className={"FormJson None_Display"}>
+        <FormJson />
+      </div>
+    </div>
   );
 }
 
